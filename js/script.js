fafb469ba0cb82b0804f649d5b8ea4aeff68de75
eam.dayan <eam.dayan@gmail.com>
2021-04-25 00:39:59 +0700

    time display works

--- js/script.js
+++ js/script.js
@@ -9,37 +9,65 @@ let amountOwedLabel = document.getElementById('amount-owed')
 
 const MINUTES_PER_HOUR = 60
 
-// ===================================================
-// ====2. MAIN =========================
-let liveTime = setInterval(()=>{
-
-    document.getElementById('action-btn')
-        .onclick = start()    
-    
-},1000)
-
 
 // ===================================================
-// ==== 3. EVENT MANAGER =========================
-
-
-
-
-// ===================================================
-// ==== 4. BUTTON FUNCTIONS =========================
+// ==== BUTTON FUNCTIONS =========================
 
 let start = function(){
     let startTime = new Date("2000-01-01 3:00:00")
     let stopTime = new Date("2000-01-01 7:16:00")
 
-    internetUsage = createInternetUsage(startTime, stopTime)    
-    displayAllDataToUser(internetUsage)
+    console.log("clicked");
 
+    internetUsage = createInternetUsage(startTime, stopTime)
+    timeString = createTimeString(startTime, stopTime)
+    displayAllDataToUser(timeString, internetUsage)
+}
+
+
+// ===================================================
+// ==== DISPLAY FUNCTIONS =========================
+
+let displayAllDataToUser = function(timeString, internetUsage){
+    displayItem(startTimeLabel, timeString.startTime)
+    displayItem(stopTimeLabel, timeString.stopTime)
+    displayItem(minutesPassedLabel, internetUsage.totalTime)
+    displayItem(amountOwedLabel, internetUsage.amountOwed)
+}
+
+let displayItem = function(element, value){
+    element.innerHTML = value;
+}
+
+
+// ===================================================
+// ==== CALCULATE FUNCTIONS =========================
+
+function createTimeString(start, stop){
+    return {
+        startTime: convertToSimpleTime(start),
+        stopTime: convertToSimpleTime(stop)
+    }
+}
+
+function convertToSimpleTime(time){
+    var fullTime = time.toLocaleTimeString().split(":")
+    try{
+        secondAndPeriod = fullTime[2].split(" ")
+        fullTime[2] = secondAndPeriod[1]
+
+        time = [fullTime[0], fullTime[1]]
+        period = fullTime[2]
+    
+        fullTime = `${time.join(":")} ${period}`
+    }
+    catch (e){
+        console.log("time string index out of range: " + e);
+    }
+    return fullTime
 }
 
 function createInternetUsage(start, stop){
-
-    // let printableTime = start.toLocaleTimeString().split(":")
 
     let total = stop.getTime() - start.getTime()
     let totalMinutes = Math.floor(total / 60_000)
@@ -67,21 +95,27 @@ function calculateAmountOwed(total){
 }
 
 
+
+
 // ===================================================
-// ==== 5. DISPLAY FUNCTIONS =========================
+// ==== CONTROLLER FUNCTIONS =========================
 
-let displayAllDataToUser = function(internetUsage){
-    displayItem(startTimeLabel, internetUsage.startValue)
-    displayItem(stopTimeLabel, internetUsage.stopValue)
-    displayItem(minutesPassedLabel, internetUsage.totalTime)
-    displayItem(amountOwedLabel, internetUsage.amountOwed)
-}
-
-let displayItem = function(element, value){
-    element.innerHTML = value;
+function initializeButtons(){
+    document.getElementById('action-btn').onclick = start 
 }
 
 
+
+// ===================================================
+// ==== MAIN =========================
+
+
+
+initializeButtons()
+
+let liveTime = setInterval(()=>{
+    
+},1000)
 
 // let clear = document.getElementById('action-btn').onclick = () => {
 //     clearInterval(liveTime)
